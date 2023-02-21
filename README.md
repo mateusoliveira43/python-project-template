07e6f03b1f57daac270f3472f4a95828afe9c682
Mateus Oliveira <66965232+mateusoliveira43@users.noreply.github.com>
2023-02-21 17:34:20 -0300

    feat: Add shellcheck to project (#27)

--- README.md
+++ README.md
@@ -133,7 +133,7 @@ mypy .
 
 Python type checker configuration in [`pyproject.toml`](pyproject.toml) file, at `[tool.mypy]` section.
 
-### Linter
+### Linters
 
 To run Python linter, run
 ```
@@ -141,6 +141,11 @@ dev lint
 ```
 
 Python linter configuration in [`.prospector.yaml`](.prospector.yaml) and [`tests/.prospector.yaml`](tests/.prospector.yaml) files.
+
+To run Shell script linter, run
+```
+dev lint --shell
+```
 
 ### Code formatters
 
--- scripts/dev_cli/commands/lint.py
+++ scripts/dev_cli/commands/lint.py
@@ -3,11 +3,30 @@
 from cly.colors import print_flashy
 from cly.utils import run_command
 
-from ..config import PROSPECTOR_DIRECTORIES
+from ..config import PROJECT_ROOT, PROSPECTOR_DIRECTORIES
 
 
-def lint() -> None:
-    """Lint project source code and tests."""
-    for directory in PROSPECTOR_DIRECTORIES:
-        print_flashy(f"Linting {directory}...")
-        run_command("prospector", directory)
+def lint(shell: bool = False) -> None:
+    """
+    Lint project Python and Shell script code.
+
+    Parameters
+    ----------
+    shell : bool, optional
+        Lint project's Shell script code if True; Otherwise, lint project's
+        Python code, by default False
+
+    """
+    if shell:
+        shell_files = [
+            file.as_posix()
+            for file in PROJECT_ROOT.glob("**/*.sh")
+            if all(ignore not in file.as_posix() for ignore in [".venv/"])
+        ]
+        names = " ".join(shell_files)
+        print_flashy("Linting shell file(s)...")
+        run_command(f"shellcheck --enable=all --severity=style {names}")
+    else:
+        for directory in PROSPECTOR_DIRECTORIES:
+            print_flashy(f"Linting {directory}...")
+            run_command("prospector", directory)
