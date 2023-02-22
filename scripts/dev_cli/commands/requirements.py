8100ad4d4acfd5ff1dba125f90a4484db5eb0425
Mateus Oliveira <66965232+mateusoliveira43@users.noreply.github.com>
2023-02-22 09:52:18 -0300

    feat: Add requirements update check to CI (#34)

---
+++ scripts/dev_cli/commands/requirements.py
@@ -0,0 +1,24 @@
+"""Update Python requirements files."""
+
+from cly.colors import print_flashy
+from cly.utils import run_command
+
+from ..config import PROJECT_ROOT
+
+
+def requirements() -> None:
+    """
+    Update Python requirements files.
+
+    Runs Poetry export command inside a Docker container to update Python
+    requirements files.
+
+    """
+    base_command = (
+        f"docker container run -w /tests -v {PROJECT_ROOT}:/tests --rm "
+        "mateusoliveira43/poetry poetry export --format requirements.txt "
+        "--output"
+    )
+    print_flashy("Checking Python requirements file(s)...")
+    run_command(f"{base_command} requirements/prod.txt")
+    run_command(f"{base_command} requirements/dev.txt --with dev")
